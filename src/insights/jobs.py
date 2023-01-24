98d9677ee76e44f4424264f8dc5145fa385f4ebf
jefersongjr <jeferson_gjr@hotmail.com>
2023-01-24 22:58:10 +0000

    :sparkles: feat: implementando a função para buscar um unico tipo de trabalho

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -15,20 +15,14 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
+    jobs = read(path)
+    jobs_types = []
+    for job in jobs:
+        if job["job_type"] in jobs_types:
+            pass
+        else:
+            jobs_types.append(job["job_type"])
+    return jobs_types
     raise NotImplementedError
 
 
