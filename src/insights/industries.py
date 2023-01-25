48fa53c0484fca93eb157bec5921efbaeb586c32
jefersongjr <jeferson_gjr@hotmail.com>
2023-01-25 00:32:22 +0000

    :sparkles: feat: implementando filtro de industrias

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,17 @@
 from typing import List, Dict
 
+from src.insights import jobs
+
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    industries = jobs.read(path)
+    industry_types = []
+    for industry in industries:
+        if industry["industry"] in industry_types:
+            pass
+        else:
+            industry_types.append(industry["industry"])
+    return industry_types
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
