9a782e8bd65f20cf22d72e418ff80dbc240343ae
jefersongjr <jeferson_gjr@hotmail.com>
2023-01-25 22:25:51 -0300

    :sparkles: feat: criando função match_salary_range

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -21,47 +21,23 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    if "min_salary" not in job or "max_salary" not in job:
+        raise ValueError
+    if (
+          not str(job["min_salary"]).isnumeric()
+          or not str(job["max_salary"]).isnumeric()
+          ):
+        raise ValueError
+    if type(salary) not in [int, str]:
+        raise ValueError
+    if int(job["max_salary"]) < int(job["min_salary"]):
+        raise ValueError
+    return int(job["min_salary"]) <= int(salary) <= int(job["max_salary"])
 
 
 def filter_by_salary_range(
     jobs: List[dict],
     salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
+    
     raise NotImplementedError
