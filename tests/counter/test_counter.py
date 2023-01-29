522341e246809f71b0b81d354fd2d5946151aaec
jefersongjr <jeferson_gjr@hotmail.com>
2023-01-29 20:42:05 -0300

    :sparkles: feat: criando testes para função counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,13 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "data/jobs.csv"
+    word1 = "TEMPORARY"
+    word2 = "temporary"
+    word3 = "BiKE"
+    word4 = "bike"
+    assert (count_ocurrences(path, word1) == 76)
+    assert (count_ocurrences(path, word2) == 76)
+    assert (count_ocurrences(path, word3) == 63)
+    assert (count_ocurrences(path, word4) == 63)
