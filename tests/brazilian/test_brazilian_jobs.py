27383011d8f6efc9e32bf34bbb041e7b36a7b1aa
jefersongjr <jeferson_gjr@hotmail.com>
2023-01-29 21:22:46 -0300

    :sparkle: feat: criando teste para função brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,11 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    path = "tests/mocks/brazilians_jobs.csv"
+    keys_english = read_brazilian_file(path)
+
+    for key in keys_english:
+        assert "title" in key
+        assert "salary" in key
+        assert "type" in key
